c38fccde5a6505da34f27a6ac8f951391e321a30
ChocoLZS <chocoielzs@gmail.com>
2022-11-25 21:35:03 +0800

    ✨ finish cmake rule for tests.

--- test/test-server-parser/main.cpp
+++ test/test-server-parser/main.cpp
@@ -1,9 +1,8 @@
-#include <parser/parser.hpp>
+
 #include <gtest/gtest.h>
 #include <map>
 #include <vector>
 #include <string>
 TEST(test_server_parser, test_1) {
-  Script script;
 //   EXPECT_STREQ("Step", tokens[0]);
 }
